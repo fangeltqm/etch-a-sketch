ebfdbdaac1602ecea1e016dd2c14e225bf0ba776
fangel <fangeltung@gmail.com>
2022-11-04 09:33:42 -0700

    add footer

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const container = document.querySelector('.container');
 
 
-
 for (let i = 0; i < 256; i++) {
     const box = document.createElement('div');
     box.classList.add('box');
     container.appendChild(box);
     
 
+}
+
+
+function etch() {
+    
 }
